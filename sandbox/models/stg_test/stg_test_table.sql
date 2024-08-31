1195a67c4b369ad8471e8f0e4c0ccf047102fb78
pond45 <xtytu7fc@gmail.com>
2024-08-31 21:48:47 +0900

    dbt構築

---
+++ sandbox/models/stg_test/stg_test_table.sql
@@ -0,0 +1,14 @@
+with source as (
+      select * from {{ source('raw_test', 'test_table') }}
+),
+renamed as (
+    select
+        {{ adapter.quote("id") }},
+        {{ adapter.quote("name") }},
+        {{ adapter.quote("age") }},
+        {{ adapter.quote("sex") }}
+
+    from source
+)
+select * from renamed
+  

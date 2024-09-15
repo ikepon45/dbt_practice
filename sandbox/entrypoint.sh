efe52eccc1f22ac90cf22187ce0bb0c6f50abd8f
pond45 <xtytu7fc@gmail.com>
2024-09-15 17:57:43 +0900

    dbt docsの作成処理を追加

--- sandbox/entrypoint.sh
+++ sandbox/entrypoint.sh
@@ -1,8 +1,12 @@
 # テスト実施とモデル作成
 dbt build
 
-# テスト結果のレポート作成
+# dbt docsを作成
+dbt docs generate
+
+# テスト結果のレポートを作成
 edr report
 
 # GCSバケットにデプロイ
+gsutil -m cp -r target/* gs://dbt_documents_shingo_ikeda/dbt_docs/
 gsutil -m cp -r edr_target/* gs://dbt_documents_shingo_ikeda/elementary_report/

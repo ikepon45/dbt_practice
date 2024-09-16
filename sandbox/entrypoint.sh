4e8b846772a16c46474bd102f3cf0e73f670c6af
pond45 <xtytu7fc@gmail.com>
2024-09-17 01:04:37 +0900

    deploy

--- sandbox/entrypoint.sh
+++ sandbox/entrypoint.sh
@@ -4,15 +4,11 @@ dbt deps
 # テスト実施とモデル構築
 dbt build
 
-# dbt docsを作成
-dbt docs generate
-
 # テスト結果のレポートを作成
 edr report
 
 # サービスアカウントでGoogle Cloudにログイン
 gcloud auth activate-service-account github-actions@udemy-sql-381210.iam.gserviceaccount.com --key-file=service-account-key.json
 
-# GCSバケットにデプロイ
-gsutil -m cp -r target/* gs://dbt_documents_shingo_ikeda/dbt_docs/
+# テスト結果のレポートをGCSバケットにデプロイ
 gsutil -m cp -r edr_target/* gs://dbt_documents_shingo_ikeda/elementary_report/

538caa417248ebb688592fee56d97b01d1d3bd1e
pond45 <xtytu7fc@gmail.com>
2024-09-23 14:28:32 +0900

    修正

--- sandbox/entrypoint.sh
+++ sandbox/entrypoint.sh
@@ -4,19 +4,5 @@ dbt deps
 # テスト実施とモデル構築
 dbt build
 
-# テスト結果のレポートを作成
-# edr report
-
 # テスト結果のレポートをGCSバケットにデプロイ
 edr send-report --gcs-bucket-name dbt_documents_shingo_ikeda --bucket-file-path elementary_report/elementary_report.html
-
-# サービスアカウントでGoogle Cloudにログイン
-# gcloud auth activate-service-account --key-file=service-account-key.json
-
-# if grep -q "target: dev" profiles.yml; then
-#     # テスト結果のレポートを検証環境のGCSバケットにデプロイ
-#     gsutil -m cp -r edr_target/elementary_report.html gs://dbt_documents_shingo_ikeda/elementary_report/
-# else
-#     # テスト結果のレポートを本番環境のGCSバケットにデプロイ
-#     gsutil -m cp -r edr_target/elementary_report.html gs://dbt_documents_shingo_ikeda_prod/elementary_report/
-# fi
